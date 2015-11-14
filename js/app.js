4ffbab263483ae46f8f31039a82741f6cc1b43a6
tbauer516 <bauer.tyler7@yahoo.com>
2015-11-14 14:10:43 -0800

    made reset button work. added tests for the email and last name inputs.

--- js/app.js
+++ js/app.js
@@ -4,6 +4,10 @@ angular.module('RegistrationApp', [])
 
 .controller('RegisterCtrl', ['$scope', function($scope) {
 
-
+	$scope.clearForm = function() {
+		document.querySelector('#signUpForm').reset();
+		$scope.signUpForm.$setPristine();
+		$scope.signUpForm.$setUntouched();
+	}
 
 }])
--- test/form-spec.js
+++ test/form-spec.js
@@ -1,12 +1,69 @@
 'use strict';
 
-describe('certain thing...', function(){
+describe('Index page form', function(){
 
-  it('should...', function() {
+	beforeEach(function(){
+		browser.get('http://localhost:8000/');
+	})
 
-    expect( 'actual value' ).toEqual('expected value');
+	it('should have an invalid email address', function() {
 
-  });
+		var emailBox = element( by.id('email') );
+		emailBox.sendKeys('abc');
+		expect( emailBox.getAttribute('class') ).toContain('ng-invalid');
 
+		emailBox.clear();
+		expect( emailBox.getAttribute('class') ).toContain('ng-invalid');		
+
+	});
+
+	it('should have a valid email address', function() {
+
+		var emailBox = element( by.id('email') );
+		emailBox.sendKeys('test.test@gmail.com');
+		expect( emailBox.getAttribute('class') ).toContain('ng-valid');
+
+		emailBox.clear();
+		emailBox.sendKeys('test.test.123@email.net');
+		expect( emailBox.getAttribute('class') ).toContain('ng-valid');
+
+		emailBox.clear();
+		emailBox.sendKeys('test@q.gov');
+		expect( emailBox.getAttribute('class') ).toContain('ng-valid');
+	});
+
+	it('should have an invalid last name', function() {
+
+		var lastName = element( by.id('lastName') );
+		lastName.click();
+		var firstName = element( by.id('firstName') );
+		firstName.click();
+		expect( lastName.getAttribute('class') ).toContain('ng-invalid');
+
+		lastName.sendKeys('test');
+		lastName.clear();
+		expect( lastName.getAttribute('class') ).toContain('ng-invalid');
+
+	});
+
+	it('should have a valid last name', function() {
+
+		var lastName = element( by.id('lastName') );
+		lastName.sendKeys('test');
+		expect( lastName.getAttribute('class') ).toContain('ng-valid');
+
+		lastName.clear();
+		lastName.sendKeys('123');
+		expect( lastName.getAttribute('class') ).toContain('ng-valid');
+
+		lastName.clear();
+		lastName.sendKeys('TEST');
+		expect( lastName.getAttribute('class') ).toContain('ng-valid');
+
+		lastName.clear();
+		lastName.sendKeys('test 123 TEST');
+		expect( lastName.getAttribute('class') ).toContain('ng-valid');
+
+	});
 
 });
