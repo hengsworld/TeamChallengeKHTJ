96511555d391d5e8bab55c5ae32eb562fbb73df7
Heng <hengsworld@gmail.com>
2015-11-15 23:28:31 -0800

    Added submit

--- js/app.js
+++ js/app.js
@@ -4,10 +4,13 @@ angular.module('RegistrationApp', [])
 
 .controller('RegisterCtrl', ['$scope', function($scope) {
 
-	$scope.clearForm = function() {
-		document.querySelector('#signUpForm').reset();
-		$scope.signUpForm.$setPristine();
-		$scope.signUpForm.$setUntouched();
-	}
+    $scope.clearForm = function() {
+        document.querySelector('#signUpForm').reset();
+        $scope.signUpForm.$setPristine();
+        $scope.signUpForm.$setUntouched();
+    }
+    $scope.submitForm = function() {
+        $scope.submitted = true;
+    }
 
 }])
