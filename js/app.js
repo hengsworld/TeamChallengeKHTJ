f854baa10c544fea60ec80c39409f790729f0d80
kevin ke <kevinkdke@gmail.com>
2015-11-17 13:30:15 -0800

    date validator in progress

--- js/app.js
+++ js/app.js
@@ -13,6 +13,32 @@ angular.module('RegistrationApp', [])
         $scope.submitted = true;
     }
 
+    $scope.ageValidation = function() {
+    	var dob = document.getElementById("#numbers").val();
+    	var now = new Date();
+    	var data = dob.split("/");
+    	var born = new Date(data[2], data[0]-1, data[1]);
+    	age = get_age(born, now);
+    	console.log("#numbers");
+
+    	if(isNaN(Date.parse(data[2] + "-" + data[1] + "-" + data[0]))) {
+    		return false;
+    	}
+
+    	if(age <= 13) {
+    		alert("Age should be greater than 13");
+    		return false;
+    	}
+    }
+
+    $scope.get_age = function(born, now) {
+    	var birthday = new Date(now.getFullYear(), born.getMonth(), born.getDate());
+    	if(now >= birthday) {
+    		return now.getFullYear() - born.getFullYear();
+    	} else {
+    		return now.getFulYear() - born.getFullYear() - 1;
+    	}
+    }
 }])
 
 .directive('pwCheck', function () {
