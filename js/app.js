95063c3223dd3ea102206b168ac6e43e8f65cf99
Heng <hengsworld@gmail.com>
2015-12-02 13:53:21 -0800

    lol

--- js/app.js
+++ js/app.js
@@ -15,46 +15,41 @@ angular.module('RegistrationApp', [])
     }
 
     $scope.getDate = function(passedDate) {
-    	var today = new Date();
-    	var test = (today.getMonth() + 1) + "/"
-    		+ today.getDate() + "/" + (today.getFullYear() - 13);
-    	var past = Date.parse(test);
-    	var passed = Date.parse(passedDate);
-    	return passed < past; 
+        var today = new Date();
+        var test = (today.getMonth() + 1) + "/" + today.getDate() + "/" + (today.getFullYear() - 13);
+        var past = Date.parse(test);
+        var passed = Date.parse(passedDate);
+        return passed < past;
     }
 }])
 
-.directive("compareTo", function() {
-    return {
-        require: "ngModel",
-        scope: {
-            otherModelValue: "=compareTo"
-        },
-        link: function(scope, element, attributes, ngModel) {
-             
-            ngModel.$validators.compareTo = function(modelValue) {
-                return modelValue == scope.otherModelValue;
-            };
- 
-            scope.$watch("otherModelValue", function() {
-                ngModel.$validate();
-            });
-        }
-    };
+.directive("compareTo", function() {    
+    return {        
+        require: "ngModel",
+                scope: {            
+            otherModelValue: "=compareTo"        
+        },
+                link: function(scope, element, attributes, ngModel) {                         
+            ngModel.$validators.compareTo = function(modelValue) {                
+                return modelValue === scope.otherModelValue;            
+            };             
+            scope.$watch("otherModelValue", function() {                
+                ngModel.$validate();            
+            });        
+        }    
+    };
 })
 
-.directive("dateValid", function() {
-    return {
-        require: "ngModel",
-        link: function(scope, element, attributes, ngModel) {
-             
-            ngModel.$validators.dateValid = function(modelValue) {
-                return scope.getDate(modelValue);
-            };
- 
-            scope.$watch("modelValue", function() {
-                ngModel.$validate();
-            });
-        }
-    };
+.directive("dateValid", function() {    
+    return {        
+        require: "ngModel",
+                link: function(scope, element, attributes, ngModel) {                         
+            ngModel.$validators.dateValid = function(modelValue) {               
+                return scope.getDate(modelValue);            
+            };  
+            scope.$watch("modelValue", function() {                
+                ngModel.$validate();            
+            });        
+        }    
+    };
 });;
