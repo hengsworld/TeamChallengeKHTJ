5bdabcf8cd897fc69433e0538ddab658f2f745eb
Heng <hengsworld@gmail.com>
2015-12-05 14:19:05 -0800

    Created tags but saving into empty string

--- Todo List/js/app.js
+++ Todo List/js/app.js
@@ -121,19 +121,81 @@ angular.module('TodoApp', ['angular.panels', 'ui.router', 'ui.bootstrap', 'fireb
 // separate controller for the edit page
 .controller('newNoteCtrl', ['$scope', function($scope) {
 
-    // creates new note and appends it to firebase cloud Json
-    $scope.newNote = function() {
-        var newItem = {
-            title: $scope.title,
-            body: $scope.body,
-            author: $scope.userName,
-            time: Firebase.ServerValue.TIMESTAMP
-        };
-        $scope.$parent.list.$add(newItem);
-        $scope.$parent.list.$save();
-    }
+        // creates new note and appends it to firebase cloud Json
+        $scope.newNote = function() {
+            var newItem = {
+                title: $scope.title,
+                body: $scope.body,
+                tagText: $scope.tagText,
+                author: $scope.userName,
 
-}])
+                time: Firebase.ServerValue.TIMESTAMP
+            };
+            console.log(newItem)
+            $scope.$parent.list.$add(newItem);
+            $scope.$parent.list.$save();
+        }
+        $scope.inputTags = [];
+
+        $scope.addTag = function() {
+            if ($scope.tagText.length == 0) {
+                return;
+            }
+
+            $scope.inputTags.push({
+                name: $scope.tagText
+            });
+            $scope.tagText = '';
+        }
+
+        $scope.deleteTag = function(key) {
+            if ($scope.inputTags.length > 0 &&
+                $scope.tagText.length == 0 &&
+                key === undefined) {
+                $scope.inputTags.pop();
+            } else if (key != undefined) {
+                $scope.inputTags.splice(key, 1);
+            }
+        }
+
+    }])
+    .directive('tagInput', function() {
+        return {
+            restrict: 'A',
+            link: function(scope, element, attrs) {
+                scope.inputWidth = 20;
+
+                // Watch for changes in text field
+                scope.$watch(attrs.ngModel, function(value) {
+                    if (value != undefined) {
+                        var tempEl = $('<span>' + value + '</span>').appendTo('body');
+                        scope.inputWidth = tempEl.width() + 5;
+                        tempEl.remove();
+                    }
+                });
+
+                element.bind('keydown', function(e) {
+                    if (e.which == 9) {
+                        e.preventDefault();
+                    }
+
+                    if (e.which == 8) {
+                        scope.$apply(attrs.deleteTag);
+                    }
+                });
+
+                element.bind('keyup', function(e) {
+                    var key = e.which;
+
+                    // Tab or Enter pressed 
+                    if (key == 9 || key == 13) {
+                        e.preventDefault();
+                        scope.$apply(attrs.newTag);
+                    }
+                });
+            }
+        }
+    })
 
 .controller('sideBarCtrl', ['$scope', 'panels', function($scope, panels) {
 
@@ -143,6 +205,7 @@ angular.module('TodoApp', ['angular.panels', 'ui.router', 'ui.bootstrap', 'fireb
     });
 
 }])
+
 
 // separate controller for the login modal
 .controller('ModalInstanceCtrl', ['$scope', '$uibModalInstance', function($scope, $uibModalInstance) {
