b746fab149d387f7849cdb87a3d5c3b80b60bffe
tbauer516 <bauer.tyler7@yahoo.com>
2015-12-09 21:10:52 -0800

    fixed the issue when returning to the edit page

--- Todo List/js/app.js
+++ Todo List/js/app.js
@@ -35,7 +35,7 @@ angular.module('TodoApp', ['angular.panels', 'ui.router', 'ui.bootstrap', 'fireb
 // parent controller that houses all the ui-views
 // put all global functions and variables here to access them from
 // the other ui-views
-.controller('TodoCtrl', ['$scope', '$uibModal', '$firebaseAuth', '$firebaseArray', '$firebaseObject', '$stateParams', 'panels', function($scope, $uibModal, $firebaseAuth, $firebaseArray, $firebaseObject, $stateParams, panels) {
+.controller('TodoCtrl', ['$scope', '$uibModal', '$firebaseAuth', '$firebaseArray', '$firebaseObject', '$stateParams', 'panels', '$sce', function($scope, $uibModal, $firebaseAuth, $firebaseArray, $firebaseObject, $stateParams, panels, $sce) {
 
     //reference to the app
     var ref = new Firebase("https://khtj-todo-list.firebaseio.com/");
@@ -142,6 +142,10 @@ angular.module('TodoApp', ['angular.panels', 'ui.router', 'ui.bootstrap', 'fireb
         console.log($scope.list.$indexFor(key));
     }
 
+    $scope.renderHtml = function(html_code) {
+        return $sce.trustAsHtml(html_code);
+    }
+
 }])
 
 // separate controller for the edit page
@@ -188,6 +192,19 @@ angular.module('TodoApp', ['angular.panels', 'ui.router', 'ui.bootstrap', 'fireb
         $scope.tagText = '';
     }
 
+    $scope.tinymceOptions = {
+        mode: 'exact',
+        inline: false,
+        skin: 'lightgray',
+        theme : 'modern',
+        height: 300,
+        plugins: [
+            'advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker',
+            'searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking',
+            'save table contextmenu directionality emoticons template paste textcolor'
+        ],
+        toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media fullpage | forecolor backcolor emoticons'
+    };
 
 }])
 
@@ -237,66 +254,3 @@ angular.module('TodoApp', ['angular.panels', 'ui.router', 'ui.bootstrap', 'fireb
         $uibModalInstance.dismiss('cancel');
     };
 }]);
-
-angular.module('ui.tinymce', [])
-    .value('uiTinymceConfig', {})
-    .directive('uiTinymce', ['uiTinymceConfig', function(uiTinymceConfig) {
-    uiTinymceConfig = uiTinymceConfig || {};
-    var generatedIds = 0;
-    return {
-        require: 'ngModel',
-        link: function(scope, elm, attrs, ngModel) {
-            var expression, options, tinyInstance;
-            // generate an ID if not present
-            if (!attrs.id) {
-                attrs.$set('id', 'uiTinymce' + generatedIds++);
-            }
-            options = {
-                // Update model when calling setContent (such as from the source editor popup)
-                setup: function(ed) {
-                    ed.on('init', function(args) {
-                        ngModel.$render();
-                    });
-                    // Update model on button click
-                    ed.on('ExecCommand', function(e) {
-                        ed.save();
-                        ngModel.$setViewValue(elm.val());
-                        if (!scope.$$phase) {
-                            scope.$apply();
-                        }
-                    });
-                    // Update model on keypress
-                    ed.on('KeyUp', function(e) {
-                        console.log(ed.isDirty());
-                        ed.save();
-                        ngModel.$setViewValue(elm.val());
-                        if (!scope.$$phase) {
-                            scope.$apply();
-                        }
-                    });
-                },
-                mode: 'exact',
-                elements: attrs.id
-            };
-            if (attrs.uiTinymce) {
-                expression = scope.$eval(attrs.uiTinymce);
-            } else {
-                expression = {};
-            }
-            angular.extend(options, uiTinymceConfig, expression);
-            setTimeout(function() {
-                tinymce.init(options);
-            });
-
-
-            ngModel.$render = function() {
-                if (!tinyInstance) {
-                    tinyInstance = tinymce.get(attrs.id);
-                }
-                if (tinyInstance) {
-                    tinyInstance.setContent(ngModel.$viewValue || '');
-                }
-            };
-        }
-    };
-}]); 
